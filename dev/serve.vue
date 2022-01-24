fff612a67d91d56ec5cda66a1956bb920402519c
Willow Celeste <willow@punkave.com>
2022-01-24 13:32:19 -0500

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import PkVideo from '@/pk-video.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    PkVideo
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <pk-video />
+  </div>
+</template>
